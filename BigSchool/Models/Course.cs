3798056c639b22650680d1428b13533a4416aa1b
Zink Rau <Zink Rau@DESKTOP-281ALGC>
2019-04-09 10:13:14 +0700

    Add project files.

---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Course
+    {
+        public int Id { set; get; }
+        public bool IsCanceled { set; get; }
+        public ApplicationUser Lecturer { set; get; }
+        [Required]
+        public String LecturerId { set; get; }
+        [Required]
+        [StringLength(255)]
+        public String Place { set; get; }
+        public DateTime DateTime { set; get; }
+        public Category Category { set; get; }
+        [Required]
+        public byte CategoryId { get; set; }
+    }
+}
---
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        public int Id { set; get; }
+        [Required]
+        public string Place { set; get; }
+        [Required]
+        [FutureDate]
+        public string Date { set; get; }
+        [Required]
+        [ValidTime]
+        public string Time { set; get; }
+        [Required]
+        public byte Category { set; get; }
+        public IEnumerable<Category> Categories { set; get; }
+        public string Heading { set; get; }
+        public string Action
+        {
+            get { return (Id != 0) ? "Update" : "Create"; }
+        }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
